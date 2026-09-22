74822126d4ee0d7e6967b9b20d8da4e1a82c363c
LokosPL <58862558+LokosPL@users.noreply.github.com>
2026-09-22 18:45:44 +0200

    ui: mount ServiceOS dialog provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import { ServiceOSDialogProvider } from './components/ServiceOSDialog';
 
 const view = new URLSearchParams(window.location.search).get('view');
 if (view === 'splash') document.documentElement.classList.add('splash-mode');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ServiceOSDialogProvider><App /></ServiceOSDialogProvider>
   </React.StrictMode>
 );
